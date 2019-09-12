d6271db09e95a0951b42034c5ddf024560d79e02
18114290046 <18114290046@OLAB04-D16.academico.iesb.br>
2019-09-11 21:43:34 -0300

    Criando Atividade09.

---
+++ Atividade09/src/java/managedbeans/CadastroMB.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author 18114290046
+ */
+@ManagedBean
+@RequestScoped
+public class CadastroMB {
+
+    /**
+     * Creates a new instance of CadastroMB
+     */
+    public CadastroMB() {
+    }
+    
+    public String salvar() {
+   return "lista.xhtml";
+}
+}
